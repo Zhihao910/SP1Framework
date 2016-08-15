e17fffa2c39ab7c250be72283ecd6352570d4bb4
Haziq <wevilbear@mail.com>
2016-08-15 14:50:14 +0800

    Changed the tile

--- SP1Framework/game.cpp
+++ SP1Framework/game.cpp
@@ -202,8 +202,8 @@ void renderSplashScreen()  // renders the splash screen
 {
     COORD c = g_Console.getConsoleSize();
     c.Y /= 3;
-    c.X = c.X / 2 - 9;
-    g_Console.writeToBuffer(c, "A game in 3 seconds", 0x03);
+    c.X = c.X / 2 - 10;
+    g_Console.writeToBuffer(c, "Escape from Alcatraz", 0x03);
     c.Y += 1;
     c.X = g_Console.getConsoleSize().X / 2 - 20;
     g_Console.writeToBuffer(c, "Press <Space> to change character colour", 0x09);
